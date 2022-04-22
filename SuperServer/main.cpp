8c6e8e45a5994e07e2a252ca166f941adc01db99
Tihon <Tihon>
2022-04-23 01:09:11 +0300

    проверка

--- SuperServer/main.cpp
+++ SuperServer/main.cpp
@@ -1,10 +1,17 @@
 #include <QCoreApplication>
 #include <QtDebug>
+#include "servercontroller.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    qDebug() << "Лох";
+   ServerController* controller = ServerController::getInstance();
+
+    int a1=controller->confirmAuthorization("maksim","123");
+    int a2=controller->confirmAuthorization("maksim","1234");
+
+    Farmer* a3= controller->getUser("maksim");
+
     return a.exec();
 }
